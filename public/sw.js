6f459a7598e292d2185de139a13b9720b6681efb
Oswaldo Hernandez <20191155@uthh.edu.mx>
2022-11-30 11:22:07 -0600

    Actualizacion del SW

--- public/sw.js
+++ public/sw.js
@@ -5,14 +5,69 @@ const preLoad = function () {
     });
 };
 
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+
+const cacheinmutable = caches.open(CACHE_INMUTABLE_NAME)
+    .then(cache => {
+        return cache.addAll([
+            'https://fonts.googleapis.com/css?family=Nunito',
+            'https://use.fontawesome.com/releases/v5.6.3/css/all.css',
+            'https://fonts.googleapis.com/css2?family=Lobster&display=swap',
+            'https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap',
+            'https://fonts.googleapis.com/css2?family=Merienda:wght@700&family=Open+Sans+Condensed:wght@300&display=swap',
+        ]);
+    });
+e.respondWith(Promise.all([respuesta, cacheinmutable]));
+
 self.addEventListener("install", function (event) {
     event.waitUntil(preLoad());
 });
 
 const filesToCache = [
     '/',
-    '/offline.html'
+    '/Directorio',
+    '/Huejutla',
+    '/Contactanos',
+    '/login',
+    '/css/app.css',
+    '/css/css/all.css',
+    '/css/styleOriginal.css',
+    '/js/app.js',
+    '/js/bootstrap.min.js',
+    '/storage/uploads/lG696Wpl9T6bGN2My2cb1AfYvHQxsUdbGkTVek95.png',
+    '/storage/uploads/Huejutla.jpg',
+    '/offline.html',
+    '/offline1.html'
 ];
+
+function limpiarcache(cacheName, numeroItems){
+    caches.open(cacheName)
+    .then(cache =>{
+        return cache.keys()
+        .then (keys =>{
+            console.log(keys);
+        });
+    })
+}
+
+self.addEventListener('fetch', e=>{
+    const respuesta = caches.match(e.request)
+    .then(res=> {
+        if(res) return res;
+        console.log('no existe', e.request.url);
+
+        return fetch(e.request).then(newResp =>{
+
+            caches.open(e.request).then( newResp => {
+                cache.put(e.request, newResp)
+                limpiarcache(CACHE_DYNAMIC_NAME,5);
+            });
+            
+        });
+        return newResp.clone();
+    });
+});
 
 const checkResponse = function (request) {
     return new Promise(function (fulfill, reject) {
